d4a7232bcf888e571528e653eaf45b91cea73889
52IRL <admin@doubtlessprayer.com>
2024-10-31 08:56:13 -0500

    Add files via upload

---
+++ html5up-massively/site52irl/site52irl/site52irlApp.swift
@@ -0,0 +1,17 @@
+//
+//  site52irlApp.swift
+//  site52irl
+//
+//  Created by #52IRL on 10/31/24.
+//
+
+import SwiftUI
+
+@main
+struct site52irlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ html5up-massively/site52irl/site52irlTests/site52irlTests.swift
@@ -0,0 +1,16 @@
+//
+//  site52irlTests.swift
+//  site52irlTests
+//
+//  Created by #52IRL on 10/31/24.
+//
+
+import Testing
+
+struct site52irlTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
